23b8b883d2dd34c94d64ca53c021eeaf2097f660
Caroline Konrad <ckonrad@live.unc.edu>
2022-03-19 12:48:36 -0400

    Working on move function

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-function newImage(url, left, bottom){
+function newImage(url){
     let image = document.createElement('img')
     image.src = url
-    image.style.position = 'fixed'
-    image.style.left = left + 'px'
-    image.style.bottom = bottom + 'px'
     document.body.append(image)
     return image
 }
 
-newImage('assets/green-character.gif', 100, 250)
-newImage('assets/tree.png', 200, 450)
-newImage('assets/pillar.png', 350, 250)
-newImage('assets/pine-tree.png', 450, 350)
-newImage('assets/crate.png', 150, 350)
-newImage('assets/well.png', 500, 575)
+function move(image) {
+    image.style.position = 'fixed'
 
+    function moveToCoordinates(left, bottom) {
+        image.style.left = left + 'px'
+        image.style.bottom = bottom + 'px'
+    }
 
-function newItem(url, left, bottom){
-    let item = newImage(url, left, bottom)
+    return {
+        to: moveToCoordinates
+    }
+}
+
+move(newImage('assets/green-character.gif')).to(100, 350)
+move(newImage('assets/tree.png')).to(300, 250)
+move(newImage('assets/pillar.png')).to(200, 550)
+move(newImage('assets/pine-tree.png')).to(200, 100)
+move(newImage('assets/crate.png')).to(100, 200)
+move(newImage('assets/well.png')).to(100, 175)
+
+function newItem(url){
+    let item = newImage(url)
     item.addEventListener('click', () => {
         item.remove()
         let inventoryItem = document.createElement('img')
@@ -25,11 +34,13 @@ function newItem(url, left, bottom){
         inventory.append(inventoryItem)
     })
     return item
-}
+    }
+
+
 
 newItem('assets/sword.png', 500, 555)
-newItem('assets/shield.png', 165, 335)
-newItem('assets/staff.png', 600, 250)
+newItem('assets/shield.png', 500, 300)
+newItem('assets/staff.png', 400, 500)
 
 function newInventory(){
     let inventory = document.createElement('div')
@@ -49,3 +60,4 @@ function newInventory(){
 }
 
 const inventory = newInventory()
+
